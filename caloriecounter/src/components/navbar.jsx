9b53dfaae20f5de77629434c409a6e93cee87b14
Ralchev <ralchev.nikola@gmail.com>
2025-05-19 17:22:45 +0300

    Initiial HomePAge

---
+++ caloriecounter/src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+import "../css/navbar.css"
+
+function NavBar(){
+    return <nav className="navbar">
+        <div className = "navbar-brand">
+            <Link to = "/"> Movie App </Link>
+        </div>
+        <div className = "navbar-links">
+            <Link to = "/" className="nav-link-home">Home</Link>
+            <Link to = "/favourites" className="nav-link-favourites">Favourites</Link>
+        </div>
+    </nav>
+}
+
+export default NavBar
